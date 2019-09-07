00f796e009961f841725078bde52dbf90aacc5cf
Nishida Satoshi <westricefield@icloud.com>
2019-09-07 21:44:07 +0900

    add 'age_matome'

---
+++ age_matome.py
@@ -0,0 +1,14 @@
+text = input('年齢入力')
+if text.isdigit():
+    age = int(text)
+    if age < 20:
+        if age < 6:
+            print('未成年(幼児)')
+        elif age >= 6 and age <= 15:
+            print('未成年(義務教育期間)')
+        else:
+            print('未成年')
+    elif age < 65:
+        print('成人')
+    else:
+        print('高齢者')
